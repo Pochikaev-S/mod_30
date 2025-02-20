de63a11282dc89c53a182441934aa1d170e6b130
Pochikaev-S <s.pochikaev@gmail.com>
2025-02-20 18:58:48 +0400

    Create create_data.py

---
+++ src/create_data.py
@@ -0,0 +1,29 @@
+import random
+
+import faker
+
+from models import Client
+
+faker = faker.Faker("en_US")
+
+
+def get_num(lenght):
+    return "".join(chr(random.randint(49, 57)) for _ in range(lenght))
+
+
+def get_str(lenght):
+    return "".join(chr(random.randint(97, 122)) for _ in range(lenght))
+
+
+def create_db_client(num):
+    client_list = []
+    for _ in range(num):
+        name = faker.first_name()
+        surname = faker.last_name()
+        credit_card = f'{"-".join(get_num(4) for _ in range(4))}'
+        car_number = f"{get_str(1)}{get_num(3)}{get_str(2)}163"
+        client = Client(
+            name=name, surname=surname, credit_card=credit_card, car_number=car_number
+        )
+        client_list.append(client)
+    return client_list
