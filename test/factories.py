aef796a6092e4b9231984893a0af1fb8cfff59e1
Pochikaev-S <s.pochikaev@gmail.com>
2025-02-20 19:18:44 +0400

    Create factories.py

---
+++ test/factories.py
@@ -0,0 +1,28 @@
+import random
+
+import factory
+import factory.fuzzy as fuzzy
+from module_30_ci_linters.homework.hw1.src.app import db
+from module_30_ci_linters.homework.hw1.src.models import Client, Parking
+
+
+class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Client
+        sqlalchemy_session = db.session
+
+    name = factory.Faker("first_name")
+    surname = factory.Faker("last_name")
+    credit_card = factory.LazyAttribute(lambda x: random.randrange(0, 1))
+    car_number = fuzzy.FuzzyText(prefix="№-")
+
+
+class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Parking
+        sqlalchemy_session = db.session
+
+    address = factory.Faker("address")
+    opened = factory.LazyAttribute(lambda x: random.randrange(0, 1))
+    count_places = factory.LazyAttribute(lambda x: random.randrange(5, 10))
+    count_available_places = 2
