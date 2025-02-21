61582c71ab35fa7fa38b629002d8031f53633676
Pochikaev-S <s.pochikaev@gmail.com>
2025-02-21 22:32:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from module_30_ci_linters.homework.hw1.src.app import create_app
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
     app.run(debug=True, port=8080)
   
